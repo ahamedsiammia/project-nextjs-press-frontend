6e1d85eda4ca0596f707bf47c80a7cceb6c9e83e
ahamedsiammia <md7559263@gmail.com>
2026-07-16 16:24:08 +0600

    added more text register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -4,6 +4,7 @@ const RegisterPage = () => {
     return (
         <div>
             this is register page
+            <h1>Plage register your account in our system</h1>
         </div>
     );
 };
